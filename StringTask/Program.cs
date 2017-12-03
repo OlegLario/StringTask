cbe3ae3d7926a398912145798fad60ec86084bd8
Oleg <ollarilari@gmail.com>
2017-12-03 21:42:37 +0200

    Add project files.

---
+++ StringTask/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringLogic item = new StringLogic();
+            Console.Write("Введите строку: ");
+            string someString = Console.ReadLine();
+            string modifiedString;
+            Console.WriteLine($"Начальное количество символов: {someString.Length}");
+            modifiedString = item.RemoveStringElements(someString);
+            Console.WriteLine(modifiedString);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StringTask/StringLogic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringTask
+{
+    class StringLogic
+    {
+        public string RemoveStringElements(string someString)
+        {
+            someString.Remove(1, 3);
+
+            return someString;
+        }
+        
+    }
+}
